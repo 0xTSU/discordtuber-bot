521f47f36050c6f8405b0e0a58a21100e4982887
haru <77024043+haaaaruuuu@users.noreply.github.com>
2021-05-16 16:13:33 -0700

    Update channel.py

--- channel.py
+++ channel.py
@@ -1,127 +1,163 @@
 import requests
-
 from bs4 import BeautifulSoup
-
 import json
 
-import os
-from dotenv import load_dotenv
-
-channel_str = ""
-
 class channelTube():
-    def __init__(self):
+    def __init__(self, channel_id = None):
+
         self.headers = {"User-Agent": "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"}
-        self.json_soup = None
-        self.rss_feed = None
-        self.about = None
-        self.video_page = None
+        self.channel_id = channel_id
+        self.crawlChannel(channel_id)
 
-        # Channel
-        self.channel_name = None
-        self.avatar = None
-        self.subs = None
+        # Channel 
+        channel_dict = self.setNameAndSubs()
+        self.channel_name = channel_dict[0]
+        self.subs = channel_dict[1]
+        self.avatar = self.setAvatar()
 
-        # About
-        self.total_views = None
+        # About Page
+        self.total_views = self.setTotalViews()
 
         # Recent Video
-        self.vid_title = None
-        self.vid_thumb = None
-        self.vid_link = None
+        self.vid_title = None # setVid()
+        self.vid_thumb = None # setVid()
+        self.vid_link = None # setVid()
+        self.setVid()
+        self.vid_pubdate = self.setVidPub()
+        self.vid_desc = self.setVidDesc()
 
         # Live Stream
-        self.is_live = False
-        self.live_status = None
-        self.live_link = None
-        self.live_thumb = None
+        self.live_index_num = None
+        self.is_live = self.setLiveStatus()
+        if self.is_live:
+            self.setLiveDetails()
+            
+        else:
+            self.live_status = None # setLiveDetails()
+            self.live_link = None # setLiveDetails()
+            self.live_thumb = None # setLiveDetails()
+            self.live_index_num = None # setLiveDetails()
 
-        # XML
-        self.vid_pubdate = None
-        self.vid_desc = None
-        self.vid_views = None
-        self.vid_index_num = None
+        # Video Page
+        self.crawlVideo()
+        self.vid_views = self.setVidViews()
 
-    def setChannel(self):
+    def setNameAndSubs(self):
+        channel_dict = []
+        channel_dict.append(self.json_soup['header']['c4TabbedHeaderRenderer']['title'])
+        channel_dict.append(self.json_soup['header']['c4TabbedHeaderRenderer']['subscriberCountText']['simpleText'])
+        return channel_dict
 
-        self.channel_name = self.json_soup['header']['c4TabbedHeaderRenderer']['title']
-        self.subs = self.json_soup['header']['c4TabbedHeaderRenderer']['subscriberCountText']['simpleText']
-        #todo make exception for community tab
-        self.total_views = self.about['contents']['twoColumnBrowseResultsRenderer']['tabs'][6]['tabRenderer']['content']['sectionListRenderer']['contents'][0]['itemSectionRenderer']['contents'][0]['channelAboutFullMetadataRenderer']['viewCountText']['simpleText']
+    def setTotalViews(self):
+        tab_index = self.about['contents']['twoColumnBrowseResultsRenderer']['tabs']
+        for i in range(len(tab_index)):
+            if tab_index[i]['tabRenderer']['title'] == "About":
+                total_views = tab_index[i]['tabRenderer']['content']['sectionListRenderer']['contents'][0]['itemSectionRenderer']['contents'][0]['channelAboutFullMetadataRenderer']['viewCountText']['simpleText']
+                break
+        return total_views
 
+    def setAvatar(self):
         avatar = self.json_soup['header']['c4TabbedHeaderRenderer']['avatar']['thumbnails']
-
         for i in range(len(avatar)):
             if (i == len(avatar) - 1):
-                self.avatar = avatar[i]['url']
+                avatar = avatar[i]['url']
+        return avatar
+
+    def setLiveStatus(self): 
+        x = False
+        video_index = self.json_soup['contents']['twoColumnBrowseResultsRenderer']['tabs'][1]['tabRenderer']['content']['sectionListRenderer']['contents'][0]['itemSectionRenderer']['contents'][0]['gridRenderer']['items']
+        for i in range(len(video_index) - 1):
+            status = video_index[i]['gridVideoRenderer']['thumbnailOverlays'][0]['thumbnailOverlayTimeStatusRenderer']['style']
+            if status == "LIVE":
+                self.live_index_num = i
+                x = True
+                break
+        return x
+
+    def setLiveDetails(self):
+        video_index = self.json_soup['contents']['twoColumnBrowseResultsRenderer']['tabs'][1]['tabRenderer']['content']['sectionListRenderer']['contents'][0]['itemSectionRenderer']['contents'][0]['gridRenderer']['items']
+        self.live_status = video_index[self.live_index_num]['gridVideoRenderer']['thumbnailOverlays'][0]['thumbnailOverlayTimeStatusRenderer']['style']
+        thumbnail_index = video_index[self.live_index_num]['gridVideoRenderer']['thumbnail']['thumbnails']
+        self.live_thumb = thumbnail_index[len(thumbnail_index) - 1]['url']
+        self.live_link = "https://youtube.com/watch?v=" + video_index[self.live_index_num]['gridVideoRenderer']['videoId']
+
 
     def setVid(self):
-        video_index = self.json_soup['contents']['twoColumnBrowseResultsRenderer']['tabs'][1]['tabRenderer']['content']['sectionListRenderer']['contents'][0]['itemSectionRenderer']['contents'][0]['gridRenderer']
+        video_index = self.json_soup['contents']['twoColumnBrowseResultsRenderer']['tabs'][1]['tabRenderer']['content']['sectionListRenderer']['contents'][0]['itemSectionRenderer']['contents'][0]['gridRenderer']['items']
         
-        for i in range(len(video_index['items']) - 1):
-
-            vid_status = video_index['items'][i]['gridVideoRenderer']['thumbnailOverlays'][0]['thumbnailOverlayTimeStatusRenderer']['style']
-            thumbnail_index = video_index['items'][i]['gridVideoRenderer']['thumbnail']['thumbnails']
-
-            if vid_status == "LIVE":
-
-                self.live_status = vid_status
-                self.is_live = True
-                self.live_link = "https://www.youtube.com/watch?v=" + video_index['items'][i]['gridVideoRenderer']['videoId']
-                for u in range(len(thumbnail_index)):
-                    if u == len(thumbnail_index) - 1:
-                        self.live_thumb = thumbnail_index[u]['url']
-
-            if i == 0 and vid_status == "UPCOMING": 
-
-                if self.live_status == "LIVE":
-                    self.live_status = vid_status
-
-                self.is_live = False
-
+        for i in range(len(video_index) - 1):
+            vid_status = video_index[i]['gridVideoRenderer']['thumbnailOverlays'][0]['thumbnailOverlayTimeStatusRenderer']['style']
             if vid_status == "DEFAULT":
-
-                self.vid_title = video_index['items'][i]['gridVideoRenderer']['title']['runs'][0]['text']
-                self.vid_link = "https://www.youtube.com/watch?v=" + video_index['items'][i]['gridVideoRenderer']['videoId']
                 self.vid_index_num = i
-                for u in range(len(thumbnail_index)):
-                    if u == len(thumbnail_index) - 1:
-                        self.vid_thumb = thumbnail_index[u]['url']
                 break
 
-    def setVidDetails(self):
+        thumbnail_index = video_index[self.vid_index_num]['gridVideoRenderer']['thumbnail']['thumbnails']
+        self.vid_title = video_index[self.vid_index_num]['gridVideoRenderer']['title']['runs'][0]['text']
+        self.vid_link = "https://www.youtube.com/watch?v=" + video_index[self.vid_index_num]['gridVideoRenderer']['videoId']
+        self.vid_thumb = thumbnail_index[len(thumbnail_index) - 1]['url']
+
+    def setVidDesc(self):
         desc_ind = self.rss_feed.find_all(('media:description'))
         for i in range(len(desc_ind)):
             if i == self.vid_index_num:
-                self.vid_desc = desc_ind[i]
-        
+                desc = desc_ind[i]
+                break
+        return desc.text
+    
+    def setVidPub(self):
         pub_ind = self.rss_feed.find_all('published')
         for i in range(len(pub_ind)):
             if i == self.vid_index_num:
-                self.vid_pubdate = pub_ind[i]
+                pubdate = pub_ind[i]
+                break
+        return pubdate.text
 
-    def crawl(self, channel_id):
-        url = "https://www.youtube.com/channel/" + channel_id + "/videos"
+    def setVidViews(self):
+        x = self.video_page['contents']['twoColumnWatchNextResults']['results']['results']['contents'][0]['videoPrimaryInfoRenderer']['viewCount']['videoViewCountRenderer']['viewCount']['simpleText']
+        return x
+
+    def crawlChannel(self, channel_id):
+        if channel_id[:2] == "UC":   
+            url = "https://www.youtube.com/channel/" + channel_id + "/videos"
+            request = requests.get(url, headers=self.headers)
+            # todo. kiara's channel messes it up
+            self.json_soup = json.loads(request.text.split('var ytInitialData = ')[1].split(';')[0])
+
+            url = "https://www.youtube.com/feeds/videos.xml?channel_id=" + channel_id
+            request = requests.get(url, headers=self.headers)
+            self.rss_feed = BeautifulSoup(request.text, 'lxml')
+
+            url = "https://www.youtube.com/channel/" + channel_id + "/about"
+            request = requests.get(url, headers=self.headers)
+            self.about = json.loads(request.text.split('var ytInitialData = ')[1].split(';')[0])
+
+    def crawlVideo(self):
+        url = self.vid_link
         request = requests.get(url, headers=self.headers)
-        # todo. kiara's channel messes it up
-        self.json_soup = json.loads(request.text.split('var ytInitialData = ')[1].split(';')[0])
+        self.video_page = json.loads(request.text.split('var ytInitialData = ')[1].split(';')[0])
 
-        url = "https://www.youtube.com/feeds/videos.xml?channel_id=" + channel_id
-        request = requests.get(url, headers=self.headers)
-        self.rss_feed = BeautifulSoup(request.text, 'lxml')
-
-        url = "https://www.youtube.com/channel/" + channel_id + "/about"
-        request = requests.get(url, headers=self.headers)
-        self.about = json.loads(request.text.split('var ytInitialData = ')[1].split(';')[0])
-
-        self.setChannel()
-        self.setVid()
-        self.setVidDetails()
         
+    def update(self):
+        self.crawlChannel(self.channel_id)
+        self.crawlVideo()
 
-if __name__ == "__main__":
-    example = channelTube()
+        channel_dict = self.setNameAndSubs()
+        self.channel_name = channel_dict[0]
+        self.subs = channel_dict[1]
+        self.avatar = self.setAvatar()
+        self.total_views = self.setTotalViews()
+        self.setVid()
+        self.vid_pubdate = self.setVidPub()
+        self.vid_desc = self.setVidDesc()
+        self.is_live = self.setLiveStatus()
+        if self.is_live:
+            self.setLiveDetails()
+        else:
+            self.live_status = None # setLiveDetails()
+            self.live_link = None # setLiveDetails()
+            self.live_thumb = None # setLiveDetails()
+            self.live_index_num = None # setLiveDetails()
 
-    example.crawl(channel_str)
-    print(example.vid_thumb)
-
+        # Video Page
+        self.crawlVideo()
+        self.vid_views = self.setVidViews()
